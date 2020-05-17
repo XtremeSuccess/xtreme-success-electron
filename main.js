fe00063c2ac054cdbf06e8a5634daad76fa46bb1
Aurghyadip <aurghya.blog@gmail.com>
2020-05-17 19:30:26 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        backgroundColor: '#FFFFFF',
+        icon: `file://${__dirname}/dist/xtreme-success-electron/assets/logo.png`
+    });
+
+    win.loadURL(`file://${__dirname}/dist/xtreme-success-electron/index.html`);
+    // Dev Tools
+    win.webContents.openDevTools();
+
+    //Event after window is closed
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (win == null) {
+        createWindow();
+    }
+})
